3cf4ca253bd79ad6ab2c51336655b22564daf76b
Shohjahon <shohjahon.baxtiyorov.11@gmail.com>
2026-05-20 16:01:35 +0500

    new

---
+++ app/components/Vers.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Vers() {
+  return (
+    <div className="max-w-md mx-auto mt-20 p-6 bg-white rounded-xl shadow">
+      <h1 className="text-2xl font-bold mb-6 text-black">Verselda deployni teshkirish</h1>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import LoginForm from './components/Router';
+import Vers from './components/Vers';
 
 export default function Home() {
   return (
     <div>
       <LoginForm />
+      <Vers />
     </div>
   );
 }
